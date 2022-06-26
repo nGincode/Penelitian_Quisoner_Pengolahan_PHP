0581b97fd123256bede8a0bd1257e73e9bea62f4
fembi19 <fembinurilham@gmail.com>
2022-06-26 19:20:03 +0700

    fix

--- hasil/index.php
+++ hasil/index.php
@@ -6,9 +6,115 @@
     <link rel="icon" href="../icon.jpg" sizes="16x16">
     <link rel="stylesheet" type="text/css" href="../style.css">
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+    <style>
+        .loaderbg {
+            position: fixed;
+            width: 100%;
+            z-index: 9999;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-image: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%);
+        }
+
+        .loader {
+            width: 21em;
+            height: 10em;
+            position: relative;
+            overflow: hidden;
+        }
+
+        .loader::before,
+        .loader::after {
+            content: '';
+            position: absolute;
+            bottom: 0;
+        }
+
+        .loader::before {
+            width: inherit;
+            height: 0.2em;
+            background-color: hsla(0, 0%, 85%);
+        }
+
+        .loader::after {
+            box-sizing: border-box;
+            width: 50%;
+            height: inherit;
+            border: 0.2em solid hsla(0, 0%, 85%);
+            border-radius: 50%;
+            left: 25%;
+        }
+
+        .loader span {
+            position: absolute;
+            width: 5%;
+            height: 10%;
+            background-color: white;
+            border-radius: 50%;
+            bottom: 0.2em;
+            left: -5%;
+            animation: 2s linear infinite;
+            transform-origin: 50% -3em;
+            animation-name: run, rotating;
+        }
+
+        .loader span:nth-child(2) {
+            animation-delay: 0.075s;
+        }
+
+        .loader span:nth-child(3) {
+            animation-delay: 0.15s;
+        }
+
+        @keyframes run {
+            0% {
+                left: -5%;
+            }
+
+            10%,
+            60% {
+                left: calc((100% - 5%) / 2);
+            }
+
+            70%,
+            100% {
+                left: 100%;
+            }
+        }
+
+        @keyframes rotating {
+
+            0%,
+            10% {
+                transform: rotate(0deg);
+            }
+
+            60%,
+            100% {
+                transform: rotate(-1turn);
+            }
+        }
+    </style>
+    <script>
+        window.onload = function() {
+            //hide the preloader
+            document.querySelector(".loaderbg").style.display = "none";
+        }
+    </script>
 </head>
 
 <body>
+    <div class="loaderbg">
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
     <div class="login-root">
         <div class="box-root flex-flex flex-direction--column" style="min-height: 100vh;flex-grow: 1;">
             <div class="loginbackground box-background--white padding-top--64">
@@ -59,7 +165,6 @@
             </div>
         </div>
     </div>
-
 
     <style>
         sup {
