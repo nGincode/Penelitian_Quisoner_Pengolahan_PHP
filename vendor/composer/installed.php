6dd99d507323db301336e5afe8e1e21d2db06ef3
fembi19 <fembinurilham@gmail.com>
2022-08-14 21:50:36 +0700

    fix

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '347b4dd5a2de23ae15c12677c3ee11650296b2a5',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '347b4dd5a2de23ae15c12677c3ee11650296b2a5',
+            'dev_requirement' => false,
+        ),
+        'halaxa/json-machine' => array(
+            'pretty_version' => '1.1.1',
+            'version' => '1.1.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../halaxa/json-machine',
+            'aliases' => array(),
+            'reference' => 'b4086dfe72670df060d2bd481f01bc9e450a5640',
+            'dev_requirement' => false,
+        ),
+    ),
+);
